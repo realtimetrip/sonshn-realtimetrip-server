4871c4526b348c5f268c89b5b909646944699564
sonshn <sonshn@naver.com>
2024-07-24 16:10:51 +0900

    feat(Global): 기본 Response, Exception 추가

---
+++ src/main/java/com/venti/realtimetrip/global/response/BaseResponse.java
@@ -0,0 +1,43 @@
+package com.venti.realtimetrip.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.venti.realtimetrip.global.status.BaseResponseStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.venti.realtimetrip.global.status.BaseResponseStatus.SUCCESS;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class BaseResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String message;
+    private final int code;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    // 요청에 성공한 경우
+    public BaseResponse(T result) {
+        this.isSuccess = SUCCESS.isSuccess();
+        this.message = SUCCESS.getMessage();
+        this.code = SUCCESS.getCode();
+        this.result = result;
+    }
+
+    // 요청에 실패한 경우
+    public BaseResponse(BaseResponseStatus status) {
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.code = status.getCode();
+    }
+
+    public BaseResponse(Boolean isSuccess, String message, int code) {
+        this.isSuccess = isSuccess;
+        this.message = message;
+        this.code = code;
+    }
+}
---
+++ src/main/java/com/venti/realtimetrip/global/status/BaseResponseStatus.java
@@ -0,0 +1,45 @@
+package com.venti.realtimetrip.global.status;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum BaseResponseStatus {
+
+    /**
+     * 200 : 요청 성공
+     */
+    SUCCESS(true, HttpStatus.OK.value(), "요청에 성공하였습니다."),
+
+    /**
+     * Request 오류, Response 오류
+     * 400: BAD REQUEST
+     * 401: UNAUTHORIZED
+     * 403: FORBIDDEN
+     * 404: NOT FOUND
+     */
+    // Common
+    REQUEST_ERROR(false, HttpStatus.BAD_REQUEST.value(), "입력값을 확인해주세요."),
+    EMPTY_JWT(false, HttpStatus.UNAUTHORIZED.value(), "JWT를 입력해주세요."),
+    INVALID_JWT(false, HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 JWT입니다."),
+    INVALID_USER_JWT(false,HttpStatus.FORBIDDEN.value(),"권한이 없는 유저의 접근입니다."),
+    RESPONSE_ERROR(false, HttpStatus.NOT_FOUND.value(), "값을 불러오는데 실패하였습니다."),
+
+    /**
+     * Database, Server 오류
+     * 500: INTERNAL SERVER ERROR
+     */
+    DATABASE_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "데이터베이스 연결에 실패하였습니다."),
+    SERVER_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버와의 연결에 실패하였습니다.");
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+
+}
